7ee9fc2ef1f9632539b04c6c6f72d28288d4731c
kkhanna15 <kanvikhanna@gmail.com>
2016-11-20 20:53:49 -0800

    deleting text

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("xxx")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
