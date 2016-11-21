f599e1c9470d520c97055a918f68da023c202fe6
prathyusha makam <prathyusha44m@gmail.com>
2016-11-20 20:49:35 -0800

    version 1

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("xxx")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
